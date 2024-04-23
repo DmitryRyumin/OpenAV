c4c435ce3194414fc9a8f6c133facdbe426a0617
Dmitry Ryumin <neweraairesearch@gmail.com>
2024-04-23 22:04:24 +0300

    Summary

--- openav/modules/dataset_recording/app.py
+++ openav/modules/dataset_recording/app.py
@@ -1,67 +1,66 @@
-from flask import Flask, request, render_template, jsonify, send_from_directory, send_file
+from flask import Flask, request, render_template, jsonify, send_file
 import os
 import subprocess
 import csv
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 global video_path, output_dir
 global processing_finished
 
 processing_finished = False
-video_path = 'temp_dir/video.mp4'
-output_dir = 'cuted'
+video_path = "temp_dir/video.mp4"
+output_dir = "cuted"
 
 
 def read_questions_from_csv():
     questions = []
-    with open('questions.csv', mode='r', newline='', encoding='utf-8') as file:
+    with open("questions.csv", mode="r", newline="", encoding="utf-8") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            question_number = int(row['QuestionNumber'])
-            question_text = row['QuestionText']
-            disable_time = row['Disable_time']
+            question_number = int(row["QuestionNumber"])
+            question_text = row["QuestionText"]
+            disable_time = row["Disable_time"]
             questions.append(
-                {'QuestionNumber': question_number, 'QuestionText': question_text, 'Disable_time': disable_time})
+                {"QuestionNumber": question_number, "QuestionText": question_text, "Disable_time": disable_time}
+            )
     return questions
 
 
-@app.route('/get_questions', methods=['GET'])
+@app.route("/get_questions", methods=["GET"])
 def get_questions():
     questions = read_questions_from_csv()
     return jsonify(questions=questions)
 
 
 def convert_webm_to_mp4(input_path, output_path):
-    line = ('ffmpeg -y -i ' + os.getcwd() + "/" + input_path
-            + " " + os.getcwd() + "/" + output_path)
+    line = "ffmpeg -y -i " + os.getcwd() + "/" + input_path + " " + os.getcwd() + "/" + output_path
     print(line)
     subprocess.run(line.split(" "))
 
 
 # Configure the upload folder for recorded videos
-app.config['UPLOAD_FOLDER'] = 'static/recorded-video'
+app.config["UPLOAD_FOLDER"] = "static/recorded-video"
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
 
 
-@app.route('/')
+@app.route("/")
 def index():
     global processing_finished
     processing_finished = False
-    return render_template('index_home.html')
+    return render_template("index_home.html")
 
 
-@app.route('/store_timing_data', methods=['POST'])
+@app.route("/store_timing_data", methods=["POST"])
 def store_timing_data():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.json
-        if os.path.exists('timing_data.txt'):
-            os.remove('timing_data.txt')
-        with open('timing_data.txt', 'w', encoding='utf-8') as file:
+        if os.path.exists("timing_data.txt"):
+            os.remove("timing_data.txt")
+        with open("timing_data.txt", "w", encoding="utf-8") as file:
             for item in data:
                 file.write(f"Question: {item['question']}, Timestamp: {item['timestamp']}\n")
 
@@ -69,20 +68,20 @@ def store_timing_data():
         return "Data stored successfully"
 
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload():
     global processing_finished
-    if 'video' in request.files:
-        video_file = request.files['video']
-        if video_file.filename != '':
-            video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_file.filename) + '.webm'
+    if "video" in request.files:
+        video_file = request.files["video"]
+        if video_file.filename != "":
+            video_path = os.path.join(app.config["UPLOAD_FOLDER"], video_file.filename) + ".webm"
             print(video_path)
             # video_file.save(video_path)
-            if not os.path.exists('temp_dir'):
-                os.makedirs('temp_dir')
-            video_file.save('temp_dir/video.webm')
+            if not os.path.exists("temp_dir"):
+                os.makedirs("temp_dir")
+            video_file.save("temp_dir/video.webm")
             # convert_webm_to_mp4('temp_dir/video.webm', 'temp_dir/video.mp4')
-            print('Done')
+            print("Done")
             processing_finished = True
 
             return "DONE"
@@ -96,22 +95,23 @@ def upload():
 #     print(str(processing_finished))
 #     return str(processing_finished)
 
-@app.route('/download_processed_video')
+
+@app.route("/download_processed_video")
 def download_processed_video():
-    processed_video_path = 'temp_dir/video.webm'
+    processed_video_path = "temp_dir/video.webm"
     return send_file(processed_video_path, as_attachment=True)
 
 
-@app.route('/download_timing_data')
+@app.route("/download_timing_data")
 def download_timing_data():
-    timing_data_path = 'timing_data.txt'  # Path to the file
+    timing_data_path = "timing_data.txt"  # Path to the file
     # Set cache-control headers to prevent caching
-    response = send_file(timing_data_path, as_attachment=True, mimetype='text/plain')
+    response = send_file(timing_data_path, as_attachment=True, mimetype="text/plain")
     response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # questions = read_questions_from_csv()
     # print(questions)
     app.run()
--- openav/modules/lab/video_converter.py
+++ openav/modules/lab/video_converter.py
@@ -3,6 +3,5 @@ import subprocess
 
 
 def convert_webm_to_mp4(input_path, output_path):
-    line = ('ffmpeg -i ' + os.getcwd() + "/" + input_path
-            + " " + os.getcwd() + "/" + output_path)
+    line = "ffmpeg -i " + os.getcwd() + "/" + input_path + " " + os.getcwd() + "/" + output_path
     subprocess.run(line)
