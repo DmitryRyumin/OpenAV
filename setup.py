2e76509ac2cdbb31e9d3c1529437bcfceb113952
Dmitry Ryumin <neweraairesearch@gmail.com>
2024-12-24 15:01:18 +0300

    v1.0.0-a17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-MIN_PYTHON_VERSION = (3, 9)
+MIN_PYTHON_VERSION = (3, 10)
 
 if sys.version_info[:2] < MIN_PYTHON_VERSION:
     raise RuntimeError("Python version required = {}.{}".format(MIN_PYTHON_VERSION[0], MIN_PYTHON_VERSION[1]))
@@ -10,35 +10,35 @@ if sys.version_info[:2] < MIN_PYTHON_VERSION:
 import openav
 
 REQUIRED_PACKAGES = [
-    "ipython >= 8.10.0",
-    "colorama >= 0.4.6",
-    "numpy >= 1.24.2, < 2.0.0",
-    "pandas >= 1.5.3",
-    "prettytable >= 3.6.0",
-    "torch >= 1.13.1",
-    "torchaudio >= 0.13.1",
-    "torchvision >= 0.14.1",
-    "av >= 10.0.0",
-    "filetype >= 1.2.0",
-    "vosk >= 0.3.44",
-    "requests >= 2.28.2",
-    "pyyaml >= 6.0",
-    "streamlit >= 1.20.0",
-    "watchdog >= 2.3.1",
-    "pymediainfo >= 6.0.1",
-    "pillow >= 9.5.0",
-    "imgaug >= 0.4.0",
-    "ffmpeg >= 1.4",
-    "librosa >= 0.10.1",
-    "matplotlib >= 3.6.3",
-    "mediapipe == 0.9.3.0",
-    "opencv_contrib_python >= 4.9.0.80",
-    "einops >= 0.7.0",
-    "lion_pytorch >= 0.1.4",
-    "scikit-learn >= 1.4.2",
-    "tqdm >= 4.66.2",
-    "Seaborn >= 0.13.2",
-    "Flask >= 3.0.3",
+    "ipython == 8.31.0",
+    "colorama == 0.4.6",
+    "numpy == 1.26.4",
+    "pandas == 2.2.3",
+    "prettytable == 3.12.0",
+    "torch == 2.2.2",
+    "torchaudio == 2.2.2",
+    "torchvision == 0.17.2",
+    "av == 14.0.1",
+    "filetype == 1.2.0",
+    "vosk == 0.3.44",
+    "requests == 2.32.3",
+    "pyyaml == 6.0.2",
+    "streamlit == 1.41.1",
+    "watchdog == 6.0.0",
+    "pymediainfo == 6.1.0",
+    "pillow == 11.0.0",
+    "imgaug == 0.4.0",
+    "ffmpeg == 1.4",
+    "librosa == 0.10.2.post1",
+    "matplotlib == 3.10.0",
+    "mediapipe == 0.10.20",
+    "opencv_contrib_python == 4.10.0.84",
+    "einops == 0.8.0",
+    "lion_pytorch == 0.2.3",
+    "scikit-learn == 1.6.0",
+    "tqdm == 4.67.1",
+    "seaborn == 0.13.2",
+    "Flask == 3.1.0",
 ]
 
 CLASSIFIERS = """\
@@ -54,9 +54,9 @@ Intended Audience :: Science/Research
 License :: OSI Approved :: MIT License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
 Programming Language :: Python :: 3 :: Only
 Programming Language :: Python :: Implementation :: CPython
 Topic :: Scientific/Engineering
